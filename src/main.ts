318648e4cc2232d1418a43aa2f34bde9b72d8f46
nguyenvanduocit+ <nguyenvanduocit@gmail.com>
2022-12-15 21:23:50 +0700

    fix: update

--- src/main.ts
+++ src/main.ts
@@ -1,100 +1,100 @@
-import {addIcon, MarkdownView, Notice, Plugin} from 'obsidian'
-import { SettingTab } from './SetingTab'
-import { ModalOnBoarding } from './ModalOnboarding'
-import { CanvasView, ViewType } from './CanvasView'
-import { openView } from './fns/openView'
-import { enqueue } from './queue'
+import {addIcon, MarkdownView, Menu, Notice, Plugin} from 'obsidian'
+import {SettingTab} from './SetingTab'
+import {ModalOnBoarding} from './ModalOnboarding'
+import {CanvasView, ViewType} from './CanvasView'
+import {openView} from './fns/openView'
+import {enqueue} from './queue'
 // @ts-ignore-next-line
 import logo from './logo.svg'
 
 interface PluginSetting {
-    isFirstRun: boolean
-    backgroundIndex: number
+  isFirstRun: boolean
+  backgroundIndex: number
 }
 
 const DEFAULT_SETTINGS: PluginSetting = {
-    isFirstRun: true,
-    backgroundIndex: 0
+  isFirstRun: true,
+  backgroundIndex: 0
 }
 
 export default class SharePlugin extends Plugin {
-    settings: PluginSetting
+  settings: PluginSetting
 
-    async onload() {
-        await this.loadSettings()
-        this.addSettingTab(new SettingTab(this.app, this))
+  async onload() {
+    await this.loadSettings()
+    this.addSettingTab(new SettingTab(this.app, this))
 
-        addIcon('obsidian-share', logo)
+    addIcon('obsidian-share', logo)
 
-        this.registerView(ViewType, (leaf) => {
-            return new CanvasView(leaf, this)
-        })
+    this.registerView(ViewType, (leaf) => {
+      return new CanvasView(leaf, this)
+    })
 
-        this.addRibbonIcon('obsidian-share', 'Share', async () => {
-            await openView(this.app.workspace, ViewType)
-        })
+    this.addRibbonIcon('obsidian-share', 'Share', async () => {
+      await openView(this.app.workspace, ViewType)
+    })
 
-        if (this.settings.isFirstRun) {
-            this.settings.isFirstRun = false
+    if (this.settings.isFirstRun) {
+      this.settings.isFirstRun = false
 
-            const modal = new ModalOnBoarding(this.app)
-            modal.open()
+      const modal = new ModalOnBoarding(this.app)
+      modal.open()
 
-            await openView(this.app.workspace, ViewType)
+      await openView(this.app.workspace, ViewType)
 
-            await this.saveSettings()
+      await this.saveSettings()
+    }
+
+    this.app.workspace.on('editor-menu', (menu, editor, view) => {
+      const onClick = async (isEmbed: boolean) => {
+        enqueue(editor.getSelection())
+        await openView(this.app.workspace, ViewType)
+      }
+
+      menu.addItem((item) => {
+        item.setTitle('Share as gradient')
+          .setIcon('links-coming-in')
+          .onClick(() => onClick(false))
+      })
+    })
+
+    // add command
+    this.addCommand({
+      id: 'share-as-gradient',
+      name: 'Share as gradient',
+      callback: async () => {
+        const editor =
+          this.app.workspace.getActiveViewOfType(MarkdownView)?.editor
+        const selection = editor?.getSelection()
+        if (!selection) {
+          new Notice('Please select text to share.')
+          return
         }
+        enqueue(selection)
+        await openView(this.app.workspace, ViewType)
+      }
+    })
+  }
 
-        this.app.workspace.on('editor-menu', (menu, editor, view) => {
-            const onClick = async (isEmbed: boolean) => {
-                enqueue(editor.getSelection())
-                await openView(this.app.workspace, ViewType)
-            }
+  onunload() {
+    this.app.workspace.detachLeavesOfType(ViewType)
+  }
 
-            menu.addItem((item) => {
-                item.setTitle('Share as gradient')
-                    .setIcon('links-coming-in')
-                    .onClick(() => onClick(false))
-            })
-        })
+  async loadSettings() {
+    this.settings = await this.loadData()
 
-        // add command
-        this.addCommand({
-            id: 'share-as-gradient',
-            name: 'Share as gradient',
-            callback: async () => {
-                const editor =
-                    this.app.workspace.getActiveViewOfType(MarkdownView)?.editor
-                const selection = editor?.getSelection()
-                if (!selection) {
-                    new Notice('Please select text to share.')
-                    return
-                }
-                enqueue(selection)
-                await openView(this.app.workspace, ViewType)
-            }
-        })
+    if (!this.settings) {
+      this.settings = DEFAULT_SETTINGS
     }
 
-    onunload() {
-        this.app.workspace.detachLeavesOfType(ViewType)
+    if (this.settings.backgroundIndex === undefined) {
+      this.settings.backgroundIndex = DEFAULT_SETTINGS.backgroundIndex
     }
 
-    async loadSettings() {
-        this.settings = await this.loadData()
+    await this.saveSettings()
+  }
 
-        if (!this.settings) {
-            this.settings = DEFAULT_SETTINGS
-        }
-
-        if (this.settings.backgroundIndex === undefined) {
-            this.settings.backgroundIndex = DEFAULT_SETTINGS.backgroundIndex
-        }
-
-        await this.saveSettings()
-    }
-
-    async saveSettings() {
-        await this.saveData(this.settings)
-    }
+  async saveSettings() {
+    await this.saveData(this.settings)
+  }
 }
